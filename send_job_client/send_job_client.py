18a30989703ecd0455966ff8faee757135fe403d
ljingb <ljb90@live.cn>
2014-03-10 19:44:48 +0800

    update:the client add log

--- send_job_client/send_job_client.py
+++ send_job_client/send_job_client.py
@@ -20,12 +20,11 @@ from clients import Client
 import sys,threading
 import yaml
 import time
+from loger import Logger
 
-HOSTS_LIST = None
+logger = Logger(logname='log/log.txt', loglevel=4, callfile=__file__).get_logger()
+
 PLUGIN_NAME = None
-CYCLE = None
-UPDATE = None
-DEBUG = None
 DATA_LIST = []
 IMPORT_MODULE = None
 STATUS = 0
@@ -35,120 +34,141 @@ sys.path.insert(0,sys.path[0]+'/plugin')
 def DEBUG_MODE(debug, data):
     if debug:
         print ('log:%s\n') % data
+        logger.debug(('log:%s\n') % data)
         if debug == 2:
             raw_input('DEBUG,Press Enter to continue:')
 
-def Init_Client(config_file = 'config.yaml'):
-    """初始化配置文件"""
-    global HOSTS_LIST
-    global PLUGIN_NAME
-    global CYCLE
-    global DEBUG
-    global UPDATE
+class Send_Job_Client(object):
+    def __init__(self, config_file = 'config.yaml'):
+        """初始化配置文件"""
+        global PLUGIN_NAME
+        try:
+            self.open_config = open(config_file)
+        except:
+            print 'not find cofig, please add in catalog!'
+            logger.error('not find config, please add in catalog!')
+            sys.exit()            
+        load_config = yaml.load(self.open_config)
+        try:
+            self.hosts_list = load_config['HOSTS_LIST'].split(' ')
+            self.client = Client(self.hosts_list)
+        except:
+            print 'HOSTS_LIST in config.yaml init error,please checking!'
+            logger.error('HOSTS_LIST in config.yaml init error, please checking!')
+            sys.exit()
+        try:
+            PLUGIN_NAME = load_config['PLUGIN_NAME']
+        except:
+            print 'PLUGIN_NAME in config.yaml init error,please checking!'
+            logger.error('PLUGIN_NAME in config.yaml init error, please checking!')
+            sys.exit()
+        try:
+            self.cycle = load_config['CYCLE']
+        except:
+            print 'CYCLE in config.yaml init error,please checking!'
+            logger.error('CYCLE in config.yaml init error,please checking!')
+            sys.exit()
+        try:
+            self.update = load_config['UPDATE']
+        except:
+            print 'UPDATE in config.yaml init error,please checking!'
+            logger.error('UPDATE in config.yaml init error,please checking!')
+            sys.exit()
+        try:
+            self.debug = load_config['DEBUG']
+        except:
+            print 'DEBUG in config.yaml init error,please checking!'
+            logger.error('DEBUG in config.yaml init error,please checking!')
+            sys.exit()
+        try:
+            self.log = load_config['LOG']
+        except:
+            print 'LOG in config.yaml init error,please checking!'
+            logger.error('LOG in config.yaml init error,please checking!')
+            sys.exit()
 
-    try:
-        open_config = open(config_file)
-    except:
-        print 'not find cofig, please add in catalog!'
-        sys.exit()
-    load_config = yaml.load(open_config)
-    try:
-      HOSTS_LIST = load_config['HOSTS_LIST'].split(' ')
-    except:
-        print 'HOSTS_LIST in config.yaml init error,please checking!'
-        sys.exit()
-    try:
-        PLUGIN_NAME = load_config['PLUGIN_NAME']
-    except:
-        print 'PLUGIN_NAME in config.yaml init error,please checking!'
-        sys.exit()
-    try:
-        CYCLE = load_config['CYCLE']
-    except:
-        print 'CYCLE in config.yaml init error,please checking!'
-        sys.exit()
-    try:
-        UPDATE = load_config['UPDATE']
-    except:
-        print 'UPDATE in config.yaml init error,please checking!'
-        sys.exit()
-    try:
-        DEBUG =load_config['DEBUG']
-    except:
-        print 'DEBUG in config.yaml init error,please checking!'
-        sys.exit()
+        self.open_config.close()
 
-    open_config.close()
-    return 0
+    def check_request_status(self, job_request):
+        """
+            可以增加或是使用多种方法,对出现有问题(延时过高/失败等)任务再次请求链接/放弃等处理(同步单线程长链接).
+            提示:当使用多线程异步并发的时候,注意,此时只能现实任务的提交状况,而不能获取任务之情后的反馈,此时需要在worker端进行任务完成情况
+        """
+        if job_request.complete:
+            info = "Job %s finished!  Result: %s - %s" % (job_request.gearman_job.unique,
+                                                                job_request.state, job_request.result)
+            DEBUG_MODE(self.debug, info)
+            if self.log:
+                logger.info(info)
+        elif job_request.timed_out:
+            """例如在此可以设置,如果有任务超时,则可以选择重试任务并直到成功.
+                client.wait_until_jobs_accepted(submitted_requests,wait_until_complete = True,poll_timeout = None)
+                """
+            info = "Job %s timed out!" % job_request.gearman_job.unique
+            DEBUG_MODE(self.debug, info)
+            if self.log:
+                logger.info(info)
+        elif job_request.state == JOB_UNKNOWN:
+            info = "Job %s connection failed!" % job_request.gearman_job.unique
+            DEBUG_MODE(self.debug, info)
+            if self.log:
+                logger.info(info)
 
+    def Transfer_Mode(self, data_for_process = [], back_ground = 'async'):
+        jobs_infos = {}
+        list_of_jobs = []
 
-def check_request_status(job_request):
-    """
-        可以增加或是使用多种方法,对出现有问题(延时过高/失败等)任务再次请求链接/放弃等处理(同步单线程长链接).
-        提示:当使用多线程异步并发的时候,注意,此时只能现实任务的提交状况,而不能获取任务之情后的反馈,此时需要在worker端进行任务完成情况
-    """
-    if job_request.complete:
-        print "Job %s finished!  Result: %s - %s" % (job_request.gearman_job.unique, job_request.state, job_request.result)
-    elif job_request.timed_out:
-        print "Job %s timed out!" % job_request.gearman_job.unique
-        #例如在此可以设置,如果有任务超时,则可以选择重试任务并直到成功.
-#       client.wait_until_jobs_accepted(submitted_requests,wait_until_complete = True,poll_timeout = None)
-    elif job_request.state == JOB_UNKNOWN:
-        print "Job %s connection failed!" % job_request.gearman_job.unique
-
-def Transfer_Mode(client, data_for_process = [], back_ground = 'async'):
-    jobs_infos = {}
-    list_of_jobs = []
-
-    if back_ground[1] == 'async':
-        back_ground = True
-        wait = False
-    elif back_ground[1] == 'sync':
-        back_ground = False
-        wait = True
-    elif len(back_ground) == 3 and back_ground[1] == 'stop':
-        data_for_process = [{
-            'task_name':back_ground[2],
-            'data_pack':{'SHUTDOWN': True}
-                }]
-        back_ground = False
-    else:
-        print 'transfer mode error !'
-        return sys.exit()
+        if back_ground[1] == 'async':
+            back_ground = True
+            wait = False
+        elif back_ground[1] == 'sync':
+            back_ground = False
+            wait = True
+        elif len(back_ground) == 3 and back_ground[1] == 'stop':
+            data_for_process = [{
+                'task_name':back_ground[2],
+                'data_pack':{'SHUTDOWN': True}
+                    }]
+            back_ground = False
+        else:
+            print 'transfer mode error !'
+            logger.error('transfer mode error!') 
+            return sys.exit()
     
-    if not data_for_process:
-        print "the jobs list is null!"
-        return -1
+        if not data_for_process:
+            print "the jobs list is null!"
+            logger.error('the jobs list is null!')
+            return -1
 
-    for tasks in data_for_process:
-        assert isinstance(tasks, dict)
-        DEBUG_MODE(DEBUG, tasks)
+        for tasks in data_for_process:
+            assert isinstance(tasks, dict)
+            DEBUG_MODE(self.debug, tasks)
 
-        list_of_jobs.append(dict(task=tasks['task_name'], 
-                            data=json.dumps(tasks['data_pack']),
-                            priority = PRIORITY_LOW
-                            ))
+            list_of_jobs.append(dict(task=tasks['task_name'], 
+                                data=json.dumps(tasks['data_pack']),
+                                priority = PRIORITY_LOW
+                                ))
 
-    submitted_requests = client.send_jobs(
-                                        list_of_jobs,
-                                        wait_until_complete=wait,
-                                        background=back_ground)
+        submitted_requests = self.client.send_jobs(
+                                            list_of_jobs,
+                                            wait_until_complete=wait,
+                                            background=back_ground)
 
-    print "the total job have: %d" % len(submitted_requests)
+        if self.log:
+            logger.info("the total job have: %d" % len(submitted_requests))
 
-#对任务请求后状态的监管方法如下.
-#submit_multiple_requests(jobs_requests, wait_until_complete, poll_timeout)
-#wait_until_jobs_accepted(job_requests, poll_timeout=None)
-#wait_until_jobs_completed(job_requests, poll_timeout=None)
-#以第一种方法为事例
-        
-    if DEBUG or wait:
-        completed_requests = client.wait_until_jobs_accepted(submitted_requests,poll_timeout = 2)
-        for completed_job_request in completed_requests:
-            check_request_status(completed_job_request)
-        DEBUG_MODE(DEBUG, 'job request.')
+        """对任务请求后状态的监管方法如下.
+            submit_multiple_requests(jobs_requests, wait_until_complete, poll_timeout)
+            wait_until_jobs_accepted(job_requests, poll_timeout=None)
+            wait_until_jobs_completed(job_requests, poll_timeout=None)
+            以第一种方法为事例
+        """
+        if self.debug or wait:
+            completed_requests = self.client.wait_until_jobs_accepted(submitted_requests,poll_timeout = 2)
+            for completed_job_request in completed_requests:
+                self.check_request_status(completed_job_request)
        
-    return 0
+        return 0
 
 
 class Double_Thread(threading.Thread):
@@ -160,18 +180,20 @@ class Double_Thread(threading.Thread):
         global IMPORT_MODULE
         global DATA_LIST
         global STATUS
-
         while 1:
             if self.daemon:
-                print "daemon thread,update data!"
+                if handle.log:
+                    logger.info("daemon thread,update data!")
                 DATA_LIST = IMPORT_MODULE.get_data()
             else:
-                print "undaemon thread,update data!"
+                if handle.log:
+                    logger.info("undaemon thread,upfate data!")
                 DATA_LIST = IMPORT_MODULE.get_data()
-            
-            DEBUG_MODE(DEBUG, DATA_LIST)
+
+            DEBUG_MODE(handle.debug, DATA_LIST)
+
             if STATUS == 0: 
-                time.sleep(UPDATE*60)
+                time.sleep(handle.update * 60)
             STATUS = 0
 
 def How_Use():
@@ -186,22 +208,13 @@ This progream is send clients.
          ''')
 
 if __name__=="__main__":
-    if Init_Client():
-        print "init fail!"
-        sys.exit()
+    handle = Send_Job_Client()
     try:
         IMPORT_MODULE = __import__(PLUGIN_NAME)
     except:
         print "no have this PLUGIN !"
+        logger.error("no have this PLUGIN !")
         sys.exit()
-
-    commands = {
-    'async' : Transfer_Mode,
-    'sync' : Transfer_Mode,
-    'stop' : Transfer_Mode
-        }
-
-    client = Client(HOSTS_LIST)
 
     if not len(sys.argv) >= 2:
         How_Use()
@@ -209,24 +222,24 @@ if __name__=="__main__":
     if sys.argv[1] != 'stop':
         threads = Double_Thread(True)
         threads.start()
-        time.sleep(UPDATE)
+        time.sleep(handle.update)
 
     assert isinstance(DATA_LIST, list)
     
     while 1:
         if not DATA_LIST and sys.argv[1] != 'stop':
             STATUS = -1
-            time.sleep(UPDATE)
+            time.sleep(handle.update)
             continue
-
+        
         try :
-            STATUS = commands[sys.argv[1]](client, data_for_process = DATA_LIST, back_ground = sys.argv)
+            STATUS = handle.Transfer_Mode( data_for_process = DATA_LIST, back_ground = sys.argv)
             if STATUS == -1:
-                time.sleep(UPDATE)
+                time.sleep(handle.update)
         except:
             How_Use()
             break
 
-        if float(CYCLE) == 0.0:
+        if float(handle.cycle) == 0.0:
             break
-        time.sleep(CYCLE)
+        time.sleep(handle.cycle)
